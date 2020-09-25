5e559381d798cf8b8a5cb08b4b546adbdde078c3
alimtegar <alimtegar4@gmail.com>
2020-09-25 07:56:50 +0700

    Update Monitor Function

--- back-end/helpers.js
+++ back-end/helpers.js
@@ -5,6 +5,71 @@ const prependHttp = (url) => {
     return url;
 };
 
+const extractDataFromPerformanceTiming = (timing, ...dataNames) => {
+    const navigationStart = timing.navigationStart;
+    const extractedData = {};
+
+    dataNames.forEach((name) => {
+        extractedData[name] = timing[name] - navigationStart;
+    });
+
+    return extractedData;
+};
+
+const getTimeFromPerformanceMetrics = (metrics, name) => metrics.metrics.find((x) => x.name === name).value * 1000;
+
+const extractDataFromPerformanceMetrics = (metrics, ...dataNames) => {
+    const navigationStart = getTimeFromPerformanceMetrics(metrics, 'NavigationStart');
+    const extractedData = {};
+
+    dataNames.forEach((name) => {
+        extractedData[name] = getTimeFromPerformanceMetrics(metrics, name) - navigationStart;
+    });
+
+    return extractedData;
+};
+
+const extractDataFromTracing = (path, name) => {
+    new Promise(resolve => {
+        fs.readFile(path, (err, data) => {
+            const tracing = JSON.parse(data);
+
+            const resourceTracings = tracing.traceEvents.filter(
+                x =>
+                    x.cat === 'devtools.timeline' &&
+                    typeof x.args.data !== 'undefined' &&
+                    typeof x.args.data.url !== 'undefined' &&
+                    x.args.data.url.endsWith(name)
+            );
+            const resourceTracingSendRequest = resourceTracings.find(
+                x => x.name === 'ResourceSendRequest'
+            );
+            const resourceId = resourceTracingSendRequest.args.data.requestId;
+            const resourceTracingEnd = tracing.traceEvents.filter(
+                x =>
+                    x.cat === 'devtools.timeline' &&
+                    typeof x.args.data !== 'undefined' &&
+                    typeof x.args.data.requestId !== 'undefined' &&
+                    x.args.data.requestId === resourceId
+            );
+            const resourceTracingStartTime = resourceTracingSendRequest.ts / 1000;
+            const resourceTracingEndTime =
+                resourceTracingEnd.find(x => x.name === 'ResourceFinish').ts / 1000;
+
+            fs.unlink(path, () => {
+                resolve({
+                    start: resourceTracingStartTime,
+                    end: resourceTracingEndTime,
+                });
+            });
+        });
+    });
+};
+
 module.exports = {
     prependHttp,
-}
+    extractDataFromPerformanceTiming,
+    getTimeFromPerformanceMetrics,
+    extractDataFromPerformanceMetrics,
+    extractDataFromTracing,
+};
--- back-end/index.js
+++ back-end/index.js
@@ -1,112 +1,57 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const { prependHttp } = require('./helpers');
+const {
+    prependHttp,
+    extractDataFromPerformanceTiming,
+    extractDataFromPerformanceMetrics,
+} = require('./helpers');
 
 const app = express();
 const port = 3000;
 
-const monitor = (_, res) => {
-    const sites = [
-        {
-            name: 'Baleroom',
-            url: 'https://baleroom.com',
-        },
-        {
-            name: 'Djanoer Kuning',
-            url: 'https://djanoerkuning.com',
-        },
-        {
-            name: 'Test',
-            url: 'https://alterweb.id',
-        },
-    ];
-
-    const captureScreenshot = async (url, siteName) => {
-        try {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-
-            await page.setViewport({ width: 1366, height: 768, })
-            await page.goto(url, { waitUntil: 'networkidle2' });
-
-            const pageMetrics = await page.metrics();
-
-            await page.screenshot({ path: `./public/assets/images/${siteName}.png`, });
-
-            const dataItem = {
-                url: url,
-                response_time: pageMetrics.Timestamp * 1000,
-                image: `${siteName}.png`
-            };
-
-            await browser.close();
-
-            return dataItem;
-        } catch (e) {
-            return {
-                url: url,
-                error: true,
-            }
-        }
-    };
-
-    const captureScreenshots = async () => {
-        const data = [];
-
-        for (let i = 0; i < sites.length; i++) {
-            const dataItem = await captureScreenshot(sites[i].url, sites[i].name);
-
-            data.push(dataItem);
-        }
-
-        return data;
-    }
-
-    captureScreenshots().then((data) => res.json(data));
-};
-
-const singleMonitor = (req, res) => {
+const monitor = (req, res) => {
     const url = prependHttp(req.params.url);
 
-    const captureScreenshot = async (url) => {
-        try {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
+    const getPerformanceTiming = async (url) => {
+        const browser = await puppeteer.launch().catch((err) => console.log(err));
+        const page = await browser.newPage().catch((err) => console.log(err));
 
-            await page.setViewport({ width: 1366, height: 768, })
-            await page.goto(url, { waitUntil: 'networkidle2' });
+        // Test Page
+        const client = await page.target().createCDPSession();
 
-            const pageMetrics = await page.metrics();
-            const pageTitle = await page.title();
-            const title = pageTitle.split('|')[0].trim();
+        await client.send('Performance.enable');
 
-            await page.screenshot({ path: `./public/assets/images/${title}.png`, });
+        await page.goto(url, { waitUntil: 'networkidle0' });
 
-            const dataItem = {
-                url: url,
-                title: title,
-                image: `${title}.png`,
-                response_time: pageMetrics.TaskDuration,
-            };
+        const performanceMetrics = await client.send('Performance.getMetrics');
+        const performanceTiming = JSON.parse(
+            await page.evaluate(() => JSON.stringify(window.performance.timing)).catch((err) => console.log(err))
+        );
 
-            await browser.close();
+        await browser.close().catch((err) => console.log(err));
 
-            return dataItem;
-        } catch (err) {
-            return {
-                url: url,
-                error: err,
-            }
-        }
+        const performanceTimingData = extractDataFromPerformanceTiming(
+            performanceTiming,
+            'responseEnd',
+            'domInteractive',
+            'domContentLoadedEventEnd',
+            'loadEventEnd',
+        );
+        const performanceMetricsData = extractDataFromPerformanceMetrics(
+            performanceMetrics,
+            'FirstMeaningfulPaint',
+        );
+
+        return {
+            ...performanceTimingData,
+            ...performanceMetricsData,
+        };
     };
 
-    captureScreenshot(url, '000').then((data) => res.json(data));
+    getPerformanceTiming(url).then((data) => res.json(data)).catch((err) => console.log(err));
 };
 
-// Routes
-app.get('/test', (_, res) => { res.json('test'); });
-app.get('/monitor', monitor);
-app.get('/single-monitor/:url', singleMonitor);
+app.get('/monitor/:url', monitor);
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);
